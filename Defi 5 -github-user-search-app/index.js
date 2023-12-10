ac126d78afcda8c24e07eafe5a0495ff20bfbff3
Florian <f.schaessens@gmail.com>
2023-12-10 13:43:33 +0100

    add function

--- Defi 5 -github-user-search-app/index.js
+++ Defi 5 -github-user-search-app/index.js
@@ -1,17 +1,3 @@
-/* Dark mode */
-
-const toggle = document.getElementById("moonSun");
-const html = document.querySelector("html");
-const sunMoon = document.getElementById("moon");
-
-toggle.addEventListener("click", () => {
-  const isDarkMode = html.getAttribute("data-theme") === "dark";
-  const isMoon = sunMoon.src.endsWith("icon-moon.svg");
-
-  html.setAttribute("data-theme", isDarkMode ? "light" : "dark");
-  sunMoon.src = isMoon ? "assets/icon-sun.svg" : "assets/icon-moon.svg";
-});
-
 /* API */
 
 const inputSearch = document.getElementById("search");
@@ -37,33 +23,81 @@ btnSearch.addEventListener("click", () => {
   async function getUrl() {
     const response = await fetch(urlAPI);
     const data = await response.json();
-    const dateData = data.created_at.slice(0, data.created_at.length - 10);
-    console.log(data);
+    const dateData = data.created_at
+      ? data.created_at.slice(0, data.created_at.length - 10)
+      : "Not Available";
+    const errorMessage = document.querySelector(".error-message");
 
-    picture.src = data.avatar_url;
-    userName.innerHTML = data.name;
-    userID.innerHTML = data.login;
-    date.innerHTML = `Joined ${dateData}`;
-    bio.innerHTML = data.bio;
+    if (response.ok) {
+      picture.src = data.avatar_url;
+      userName.innerHTML = data.name;
+      userID.innerHTML = data.login;
+      date.innerHTML = `Joined ${dateData}`;
+      bio.innerHTML = data.bio;
 
-    repo.innerHTML = data.public_repos;
-    followers.innerHTML = data.followers;
-    following.innerHTML = data.following;
+      repo.innerHTML = data.public_repos;
+      followers.innerHTML = data.followers;
+      following.innerHTML = data.following;
 
-    city.innerHTML =
-      data.location === "" || data.location === null
-        ? "Not Available"
-        : data.location;
-    twit.innerHTML =
-      data.twitter_username === "" || data.twitter_username === null
-        ? "Not Available"
-        : data.twitter_username;
-    github.innerHTML =
-      data.blog === "" || data.blog === null ? "Not Available" : data.blog;
-    job.innerHTML =
-      data.compagny === "" || data.compagny === null
-        ? "Not Available"
-        : data.compagny;
+      city.innerHTML =
+        data.location === "" || data.location === null
+          ? "Not Available"
+          : data.location;
+      twit.innerHTML =
+        data.twitter_username === "" || data.twitter_username === null
+          ? "Not Available"
+          : data.twitter_username;
+      github.innerHTML =
+        data.blog === "" || data.blog === null ? "Not Available" : data.blog;
+      job.innerHTML =
+        data.company === "" || data.company === null
+          ? "Not Available"
+          : data.company;
+
+      errorMessage.style.display = "none";
+    } else if (response.status === 404) {
+      errorMessage.style.display = "flex";
+    } else {
+      errorMessage.style.display = "flex";
+      console.error(
+        `GitHub API request failed with status: ${response.status}`
+      );
+    }
   }
   getUrl();
+});
+
+/* Dark mode button */
+
+const toggle = document.getElementById("moonSun");
+const html = document.querySelector("html");
+const sunMoon = document.getElementById("moon");
+const prefersDarkMode = window.matchMedia(
+  "(prefers-color-scheme: dark)"
+).matches;
+
+const enableDarkMode = () => {
+  html.setAttribute("data-theme", "dark");
+  sunMoon.src = "assets/icon-sun.svg";
+};
+
+const enableLightMode = () => {
+  html.setAttribute("data-theme", "light");
+  sunMoon.src = "assets/icon-moon.svg";
+};
+
+if (prefersDarkMode) {
+  enableDarkMode();
+} else {
+  enableLightMode();
+}
+
+toggle.addEventListener("click", () => {
+  const isDarkMode = html.getAttribute("data-theme") === "dark";
+
+  if (isDarkMode) {
+    enableLightMode();
+  } else {
+    enableDarkMode();
+  }
 });
